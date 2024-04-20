bf9b0108254748fdbbcfc7ad2cd4aa5842ab3462
Amrinder Singh <amrinder995@gmail.com>
2024-04-19 20:41:00 -0400

    remove useState and add useReducer to handle logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,24 +11,11 @@ import useApplicationData from 'hooks/useApplicationData';
 
 const App = () => {
   const {showModal, modalPhoto, toggleModal, likedPhotos, toggleFavourite, isModal, setShowModal} = useApplicationData();
-  // const [showModal, setShowModal] = useState(false);
-  // const [modalPhoto, setModalPhoto] = useState(null);
-  // const toggleModal = (photo) => {
-  //   setShowModal(true);
-  //   setModalPhoto(photo);
-  // }
-  // const isModal = showModal;
-  // const [likedPhotos, setLikedPhotos] = useState([]);
-  // const toggleFavourite = (id) => {
-  //   //add logic to update liked photo state, maybe add/remove to array containing liked photos?
-  //   const likedPhoto = likedPhotos.includes(id);
-  //   likedPhoto ? setLikedPhotos(prevLikedPhotos => prevLikedPhotos.filter(e => e !== id) ): setLikedPhotos(prevLikedPhotos => [...prevLikedPhotos, id]);
-  // }
 
   return (
     <div className="App">
       <HomeRoute photos={photos} topics={topics} toggleModal={toggleModal} likedPhotos={likedPhotos} toggleFavourite={toggleFavourite} isModal={isModal}/>
-      {showModal && <PhotoDetailsModal closeModal={() => setShowModal(false)} isModal={isModal} photo={modalPhoto} likedPhotos={likedPhotos} toggleFavourite={toggleFavourite} toggleModal={toggleModal} photos={photos}/>}
+      {showModal && <PhotoDetailsModal closeModal={() => toggleModal(null)} isModal={isModal} photo={modalPhoto} likedPhotos={likedPhotos} toggleFavourite={toggleFavourite} toggleModal={toggleModal} photos={photos}/>}
       {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key ={index}/>) } */}
     </div>
   );
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -5,21 +5,47 @@
 // The setPhotoSelected action can be used when the user selects a photo.
 // The onClosePhotoDetailsModal action can be used to close the modal.
 import { useState } from 'react';
+import { useReducer } from 'react';
+
+/* insert app levels actions below */
+export const ACTIONS = {
+  TOGGLE_MODAL: 'TOGGLE_MODAL',
+  TOGGLE_FAVOURITE: 'TOGGLE_FAVOURITE'
+};
+const initialState = {
+  showModal: false,
+  modalPhoto: null,
+  likedPhotos: []
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.TOGGLE_MODAL:
+      return {...state, showModal: action.payload !== null, modalPhoto: action.payload };
+    case ACTIONS.TOGGLE_FAVOURITE:
+      const { id } = action.payload;
+      const likedPhoto = state.likedPhotos.includes(id);
+      return {
+        ...state,
+        likedPhotos: likedPhoto ? state.likedPhotos.filter(e => e !== id) : [...state.likedPhotos, id]
+      }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
 
 
 function useApplicationData() {
-  const [showModal, setShowModal] = useState(false);
-  const [modalPhoto, setModalPhoto] = useState(null);
-  const [likedPhotos, setLikedPhotos] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const toggleModal = (photo) => {
-    setShowModal(true);
-    setModalPhoto(photo);
-  };
+    dispatch({ type: ACTIONS.TOGGLE_MODAL, payload: photo });
+  }
   const toggleFavourite = (id) => {
-    const likedPhoto = likedPhotos.includes(id);
-    likedPhoto ? setLikedPhotos(prevLikedPhotos => prevLikedPhotos.filter(e => e !== id) ): setLikedPhotos(prevLikedPhotos => [...prevLikedPhotos, id]);
-  };
-  const isModal = showModal;
+    dispatch({ type: ACTIONS.TOGGLE_FAVOURITE, payload: { id }})
+  }
+  const isModal = state.showModal;
+  const { showModal, modalPhoto, likedPhotos } = state;
   return {
     showModal,
     modalPhoto,
@@ -27,22 +53,7 @@ function useApplicationData() {
     toggleModal,
     toggleFavourite,
     isModal,
-    setShowModal
   };
 };
-
-
-// const [modalPhoto, setModalPhoto] = useState(null);
-// const toggleModal = (photo) => {
-//   setShowModal(true);
-//   setModalPhoto(photo);
-// }
-// const isModal = showModal;
-// const [likedPhotos, setLikedPhotos] = useState([]);
-// const toggleFavourite = (id) => {
-//   //add logic to update liked photo state, maybe add/remove to array containing liked photos?
-//   const likedPhoto = likedPhotos.includes(id);
-//   likedPhoto ? setLikedPhotos(prevLikedPhotos => prevLikedPhotos.filter(e => e !== id) ): setLikedPhotos(prevLikedPhotos => [...prevLikedPhotos, id]);
-// }
 
 export default useApplicationData;
